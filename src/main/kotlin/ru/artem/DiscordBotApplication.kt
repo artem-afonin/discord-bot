e219b3b951fb52afb84585259497d6390c9f51b7
Artem Afonin <artem.00.afonin@gmail.com>
2023-08-26 00:52:55 +0700

    Initial commit

---
+++ src/main/kotlin/ru/artem/DiscordBotApplication.kt
@@ -0,0 +1,11 @@
+package ru.artem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscordBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscordBotApplication>(*args)
+}
